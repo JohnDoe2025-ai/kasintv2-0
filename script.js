6c15e18c51152371b51cc38401a413598f11aa9d
KaSin TV <158577616+kasintv2-0@users.noreply.github.com>
2024-08-24 07:58:04 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ <script>
+        // JavaScript to handle the refresh button click event
+        document.getElementById('refreshButton').addEventListener('click', function() {
+            location.reload(); // Refreshes the page
+        });
+    </script>
+
+ <script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
+<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NTQsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
+<script>(function(d,a,b){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NTQsInNyYyI6Mn0=eyJ&si1='+a+'&si2='+b;d.head.appendChild(s);})(document,'subid1','subid2');</script>
+<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
+<script>(function(d,a,b){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ&si1='+a+'&si2='+b;d.head.appendChild(s);})(document,'subid1','subid2');</script>
