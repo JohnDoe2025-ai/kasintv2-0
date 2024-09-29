c24ee090ea84367f99dec751e10dc52f15c4e3e8
KaSin TV <158577616+kasintv2-0@users.noreply.github.com>
2024-09-29 13:28:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,3 @@
         });
     </script>
 
- <script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
-<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NTQsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
-<script>(function(d,a,b){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NTQsInNyYyI6Mn0=eyJ&si1='+a+'&si2='+b;d.head.appendChild(s);})(document,'subid1','subid2');</script>
-<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
-<script>(function(d,a,b){let s=d.createElement('script');s.async=true;s.src='https://zonxso.com/code/pops.js?h=waWQiOjExODYzNTUsInNpZCI6MTM2ODE5Mywid2lkIjo2MDc5NjMsInNyYyI6Mn0=eyJ&si1='+a+'&si2='+b;d.head.appendChild(s);})(document,'subid1','subid2');</script>
